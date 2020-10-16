0c447048ac683ef1869fb5f10513a55cb9cff560
SenaGust <gustavosena98@live.com>
2020-10-15 22:28:06 -0300

    Cadastro realizado com sucesso pelos services

--- js/Services/ContratanteService.js
+++ js/Services/ContratanteService.js
@@ -1,7 +1,9 @@
 const submit = (event) => {
     event.preventDefault();
 
+    const usuario = document.getElementById('inputUsuario').value;
     const nomeCompleto = document.getElementById('inputNomeCompleto').value;
+    const senha = document.getElementById('inputSenha').value;
     const endereco = document.getElementById('inputEndereco').value;
     const whatsapp = document.getElementById('inputWpp').value;
     const foto = document.getElementById('inputNomeCompleto').value;
@@ -9,24 +11,31 @@ const submit = (event) => {
     const url = 'https://localhost:44366/api/signup';
 
     const data = {
-        userID: nomeCompleto,
-        password: nomeCompleto+1+'!',
-        nomeCompleto: nomeCompleto,
-        endereco: endereco,
-        telefone: whatsapp,
-        linkFoto: foto,
-        biografia: 'qualquerCoisa',
-        tipo: "Cliente"
+      userID: usuario,
+      password: senha,
+      nomeCompleto: nomeCompleto,
+      endereco: endereco,
+      telefone: whatsapp,
+      linkFoto: foto,
+      biografia: "string",
+      tipo: "Cliente"
       };    
+    
+      const headers = {
+        'content-type': 'application/json'
+      }
 
-      const options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        data: JSON.stringify(data),
-        url,
-      };
+      console.log(JSON.stringify(data));
 
-      axios(options).then(response => console.log(response));
+      axios.post(url, data,  headers)
+      .then(response => {
+        console.log(response)
+        if(response.data.result === "Succeeded") {
+          console.log('Cadastro realizado com sucesso!')
+        } else {
+          console.log('Não foi possível realizar o cadastro!')
+        }
+      }).catch((response) => console.log('Erro desconhecido durante o cadastro do prestador', response));
 }
 
 var botao = document.getElementById("botao_cadastrar");
--- js/Services/PrestadorService.js
+++ js/Services/PrestadorService.js
@@ -1,32 +1,40 @@
 const submit = (event) => {
     event.preventDefault();
 
+    const usuario = document.getElementById('inputUsuario').value;
+    const senha = document.getElementById('inputSenha').value;
     const nomeCompleto = document.getElementById('inputNomeCompleto').value;
     const endereco = document.getElementById('inputEndereco').value;
     const whatsapp = document.getElementById('inputWpp').value;
     const foto = document.getElementById('inputNomeCompleto').value;
+    const inputBiografia = document.getElementById('inputBiografia').value;
     
     const url = 'https://localhost:44366/api/signup';
 
     const data = {
-        userID: nomeCompleto,
-        password: nomeCompleto+1+'!',
+        userID: usuario,
+        password: senha,
         nomeCompleto: nomeCompleto,
         endereco: endereco,
         telefone: whatsapp,
         linkFoto: foto,
-        biografia: 'qualquerCoisa',
-        tipo: "Cliente"
+        biografia: inputBiografia,
+        tipo: "Prestador"
       };    
 
-      const options = {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        data: JSON.stringify(data),
-        url,
-      };
+      const headers = {
+        'content-type': 'application/json'
+      }
 
-      axios(options).then(response => console.log(response));
+      axios.post(url, data,  headers)
+      .then(response => {
+        console.log(response)
+        if(response.data.result === "Succeeded") {
+          console.log('Cadastro realizado com sucesso!')
+        } else {
+          console.log('Não foi possível realizar o cadastro!')
+        }
+      }).catch((response) => console.log('Erro desconhecido durante o cadastro do prestador', response));
 }
 
 var botao = document.getElementById("botao_cadastrar");
