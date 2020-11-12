b838ce4893901920a2404afadd6b149c00ff2dd6
Nathan Ribeiro <42881774+NathanRF@users.noreply.github.com>
2020-11-11 23:53:12 -0300

    Http -> Https

--- js/Endpoints.js
+++ js/Endpoints.js
@@ -1,9 +1,9 @@
 export const MunicipiosRegiaoMetropolitanaBH = 'https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes/31030/municipios';
 
-export const Usuario = 'http://ssg-api.azurewebsites.net/api/signup';
+export const Usuario = 'https://ssg-api.azurewebsites.net/api/signup';
 
-export const Login = 'http://ssg-api.azurewebsites.net/api/login';
+export const Login = 'https://ssg-api.azurewebsites.net/api/login';
 
-export const ListarServicos = 'http://ssg-api.azurewebsites.net/api/ListarServicos';
+export const ListarServicos = 'https://ssg-api.azurewebsites.net/api/ListarServicos';
 
-export const Prestador = 'http://ssg-api.azurewebsites.net/api/Prestadores';
+export const Prestador = 'https://ssg-api.azurewebsites.net/api/Prestadores';
