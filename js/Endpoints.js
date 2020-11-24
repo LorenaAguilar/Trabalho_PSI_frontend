10c09338b1c8dfeaa8ead3d47e956c6451ccbe95
SenaGust <gustavosena98@live.com>
2020-11-23 22:53:27 -0300

    update endpoints

--- js/Endpoints.js
+++ js/Endpoints.js
@@ -1,9 +1,19 @@
+const api = 'https://ssg-api.azurewebsites.net';
+
 export const MunicipiosRegiaoMetropolitanaBH = 'https://servicodados.ibge.gov.br/api/v1/localidades/microrregioes/31030/municipios';
 
-export const Usuario = 'https://ssg-api.azurewebsites.net/api/signup';
+export const Usuario = `${api}/api/signup`;
 
-export const Login = 'https://ssg-api.azurewebsites.net/api/login';
+export const Login = `${api}/api/login`;
 
-export const ListarServicos = 'https://ssg-api.azurewebsites.net/api/ListarServicos';
+export const ListarServicos = `${api}/api/ListarServicos`;
 
-export const Prestador = 'https://ssg-api.azurewebsites.net/api/Prestadores';
+export const Prestador = `${api}/api/Prestador`;
+
+export const UnidadesDeCobranca = `${api}/api/UnidadesDeCobranca`;
+
+export const ServicosPrestados = `${api}/api/ServicosPrestados`;
+
+export const Servicos = `${api}/api/Servicos`;
+
+export const LocaisDeAtendimento = `${api}/api/LocaisDeAtendimento`;
