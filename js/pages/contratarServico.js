f4657c3a6645453dd504a66cd1d6258c08560e26
SenaGust <gustavosena98@live.com>
2020-11-27 23:08:46 -0300

    service do servico

---
+++ js/pages/contratarServico.js
@@ -0,0 +1,61 @@
+import { existeIdServicoPrestador, getIdServicoPrestador } from '../stores/ServicoPrestadorStore.js';
+import { getURL } from '../functions.js'
+import { getTodosServicos } from '../Services/ListarServicosService.js';
+
+let informations = [{
+  id: "12",
+  nome: "Leonardo Da Vinci",
+  servico: "Pintor",
+  regiao: "Sarzedo",
+  preco: "100",
+  biografia: "Ótimo pintor, especialista em desenhos e pinturas artísticas.",
+  imagem: "https://p2.trrsf.com/image/fget/cf/460/0/images.terra.com/2019/06/24/leo.jpg"
+}];
+
+if(!existeIdServicoPrestador()) {
+    getTodosServicos.then((response) => {
+      if(response.message !== 'Nenhum serviço prestado cadastrado') {
+          informations = response;    
+      }
+
+      getInformacoesServicoEscolhido();
+    });
+} else {
+  window.location.pathname = `${getURL()}/index.html`;
+}
+
+function getInformacoesServicoEscolhido() {
+  const prestador = document.getElementById('listaServicos');
+
+  prestador.innerHTML = informations
+  .filter((information) => information.id === getIdServicoPrestador())
+  .map((information) =>(`
+    <div id="containerServicoPrestado" class="row">
+      <div id="campoImagem" class="col-2">
+        <img src= ${information.imagem} />
+      </div>
+      <div id="informacoes"  class="col-10" >
+        <strong id="informacoesPrestador"> 
+          <p>
+            Nome: ${information.nome}
+          </p> 
+          <p>
+            Serviço: ${information.servico}
+          </p>
+          <p>
+            Região que atende: ${information.regiao}
+          </p>
+          <p>
+            Preço: R$ ${information.preco} (hora)
+          </p>
+          <hr />
+          <p>
+            Biografia: ${information.biografia}
+          </p>
+        </strong>
+        <button ${!EstaLogado() ? 'disabled=true' : ''} >
+            Contratar serviços
+        </button>
+      </div>
+    </div>`)).join("");
+}
