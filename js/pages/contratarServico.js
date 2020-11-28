4af0c1ff4ab8f018856764bbca78a006e6d007eb
SenaGust <gustavosena98@live.com>
2020-11-28 02:34:59 -0300

    melhorando

--- js/pages/contratarServico.js
+++ js/pages/contratarServico.js
@@ -1,6 +1,7 @@
-import { existeIdServicoPrestador, getIdServicoPrestador, setIdServicoPrestador } from '../stores/ServicoPrestadorStore.js';
+import { existeIdServicoPrestador, getIdServicoPrestador } from '../stores/ServicoPrestadorStore.js';
 import { getURL } from '../functions.js'
 import { getTodosServicos } from '../Services/ListarServicosService.js';
+import { cadastrarOrdem } from '../Services/OrdemService.js'
 
 if(existeIdServicoPrestador()) {
     getTodosServicos().then((response) => {
@@ -21,40 +22,46 @@ String.prototype.capitalize = function() {
 function getInformacoesServicoEscolhido(informations) {
   const prestador = document.getElementById('listaServicos');
 
-  informations.map((information) => {
-    if(information.prestador === getIdServicoPrestador())
-    prestador.innerHTML = (`
-      <div id="containerServicoPrestado" class="row">
-        <div id="campoImagem" class="col-2">
-          <img src= ${information.foto} />
-        </div>
-        <div id="informacoes"  class="col-10" >
-          <strong id="informacoesPrestador"> 
-            <p>
-              Nome: ${information.nomeCompleto}
-            </p> 
-            <p>
-              Serviço: ${information.categoriaServico.capitalize()}
-            </p>
-            <p>
-              Região que atende: ${information.cidade}
-            </p>
-            <p>
-              Preço: R$ ${information.preco} (hora)
-            </p>
-            <hr />
-            <p>
-              Biografia: ${information.biografia}
-            </p>
-          </strong>
-         
-        </div>
-        <div id="container_buttons">
-        <button >
-            Contratar serviços
-        </button>
-        </div>
-      </div>`
-    );
-  });
+  const information = informations
+  .filter((information) => information.servicoprestado === getIdServicoPrestador())[0];
+  
+  prestador.innerHTML = (`
+    <div id="containerServicoPrestado" class="row">
+      <div id="campoImagem" class="col-2">
+        <img src= ${information.foto} />
+      </div>
+      <div id="informacoes"  class="col-10" >
+        <strong id="informacoesPrestador"> 
+          <p>
+            Nome: ${information.nomeCompleto}
+          </p> 
+          <p>
+            Serviço: ${information.categoriaServico.capitalize()}
+          </p>
+          <p>
+            Região que atende: ${information.cidade}
+          </p>
+          <p>
+            Preço: R$ ${information.preco} (hora)
+          </p>
+          <hr />
+          <p>
+            Biografia: ${information.biografia}
+          </p>
+        </strong>
+      
+      </div>
+      <div id="container_buttons">
+      <button id="contratar">
+          Contratar serviços
+      </button>
+      </div>
+    </div>`
+  );
+  document.getElementById("contratar").onclick = (event) => {
+    event.preventDefault();
+
+    const teste = document.getElementById("inputData").value;
+  } 
 }
+
