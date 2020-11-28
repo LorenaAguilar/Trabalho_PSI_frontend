c20319bef61d6df25b2acc37088b3d4eb35f15c7
SenaGust <gustavosena98@live.com>
2020-11-27 22:27:37 -0300

    criei store prestador

---
+++ js/stores/ServicoPrestadorStore.js
@@ -0,0 +1,12 @@
+//IdServicoPrestador
+export const getIdServicoPrestador  = () => {
+    return sessionStorage.getItem('ServicoPrestador-Id');
+}
+
+export const setIdServicoPrestador  = (value) => {
+    return sessionStorage.setItem('ServicoPrestador-Id', value);
+}
+
+export const existeIdServicoPrestador  = () => {
+    return sessionStorage.getItem('ServicoPrestador-Id') !== null;
+}
